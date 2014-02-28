6ec49b2983ea6ddddf7eda97b5af84c3edf1230d
sergey <sva@ciklum.com>
2014-02-28 13:49:21 +0200

    clean

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,61 +47,6 @@ module.exports = function (grunt) {
         }
     });
 
-    grunt.registerTask('none', function() {});
-
-    grunt.registerTask('build', 'build a regular theme', function(theme, compress) {
-        var compress = compress == undefined ? true : compress;
-
-        var concatSrc;
-        var concatDest;
-        var recessDest;
-        var recessSrc;
-        var files = {};
-        var dist = {};
-
-        concatSrc = 'global_theme/build.less';
-        concatDest = 'static/themes/' + theme + '/build.less';
-        recessDest = '<%=builddir%>/static/themes/' + theme + '/bootstrap.css';
-        recessSrc = [ 'static/themes/' + theme + '/' + 'build.less' ];
-
-        dist = {src: concatSrc, dest: concatDest};
-        grunt.config('concat.dist', dist);
-        files = {}; files[recessDest] = recessSrc;
-        
-        grunt.config('recess.dist.files', files);
-        grunt.config('recess.dist.options.compress', false);
-
-        grunt.task.run(['concat', 'recess:dist', 'clean:build',
-            compress ? 'compress:'+recessDest+':'+'<%=builddir%>/static/themes/' + theme + '/bootstrap.min.css':'none']);
-    });
-
-    grunt.registerTask('compress', 'compress a generic css', function(fileSrc, fileDst) {
-        var files = {}; files[fileDst] = fileSrc;
-        grunt.log.writeln('compressing file ' + fileSrc);
-
-        grunt.config('recess.dist.files', files);
-        grunt.config('recess.dist.options.compress', true);
-        grunt.task.run(['recess:dist']);
-    });
-
-    grunt.registerTask('swatch', 'build a theme', function() {
-        var themes = fs.readdirSync(grunt.config('builddir') + '/static/themes')
-
-        grunt.file.write(grunt.config('builddir') + '/static/api.json', JSON.stringify({
-            themes: themes,
-            thumbName: 'sumbnail.png'
-        }));
-
-        themes.forEach(function (t) {
-            grunt.task.run('build:'+t);
-        });
-
-    });
-    
-    grunt.registerTask('default', 'build a theme', function() {
-        grunt.task.run('swatch');
-    });
-
     grunt.registerTask('api', 'build api', function() {
         json = {
             categories: getCategoriesJSON(),
